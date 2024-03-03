829d4e474dc1e3ae849aaffa237848dfa0cf1344
Anton Povar <povar.anton@gmail.com>
2024-03-04 00:53:37 +0200

    [+] Base classes added

--- main.py
+++ main.py
@@ -2,6 +2,56 @@ from datetime import datetime
 from collections import defaultdict, UserDict
 
 
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                break
+
+    def find_phone(self, phone):
+        return next((p for p in self.phones if p.value == phone), None)
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            raise KeyError("No such contact found.")
+
 def get_birthdays_per_week(users):
     today = datetime.today().date()
     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
